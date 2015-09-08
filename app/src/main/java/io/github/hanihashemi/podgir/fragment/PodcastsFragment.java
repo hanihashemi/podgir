03c8f03542088000d963a924bb237785b86f61d3
Hani <jhanihashemi@gmail.com>
2015-09-09 00:22:27 +0430

    add loading to podcasts fragment, add library Android-Iconics

--- app/src/main/java/io/github/hanihashemi/podgir/fragment/PodcastsFragment.java
+++ app/src/main/java/io/github/hanihashemi/podgir/fragment/PodcastsFragment.java
@@ -2,13 +2,18 @@ package io.github.hanihashemi.podgir.fragment;
 
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 import io.github.hanihashemi.podgir.App;
 import io.github.hanihashemi.podgir.R;
 import io.github.hanihashemi.podgir.activity.PodcastDetailActivity;
@@ -24,8 +29,12 @@ import io.github.hanihashemi.podgir.model.PodcastResultResponse;
 public class PodcastsFragment extends BaseFragment implements Response.Listener<PodcastResultResponse>, PodcastViewHolder.OnClick {
     @Bind(R.id.recycler_view)
     protected RecyclerView recyclerView;
+    @Bind(R.id.error)
+    FrameLayout error;
+    @Bind(R.id.progress_bar)
+    ProgressBar progressBar;
+
     private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
     private List<Podcast> podcasts;
 
     @Override
@@ -38,7 +47,7 @@ public class PodcastsFragment extends BaseFragment implements Response.Listener<
         super.customizeUI();
 
         recyclerView.setHasFixedSize(true);
-        layoutManager = new GridLayoutManager(getActivity(), 3);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
         recyclerView.setLayoutManager(layoutManager);
 
         podcasts = new ArrayList<>();
@@ -48,19 +57,30 @@ public class PodcastsFragment extends BaseFragment implements Response.Listener<
         fetchData();
     }
 
-    private void fetchData() {
+    @OnClick(R.id.retry)
+    protected void fetchData() {
+        this.error.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
         App.getInstance().addRequestToQueue(new Podcast().findAll(this, this), this);
     }
 
     @Override
     public void onResponse(PodcastResultResponse response) {
+        progressBar.setVisibility(View.GONE);
         podcasts.clear();
         podcasts.addAll(response.getPodcasts());
-        adapter.notifyDataSetChanged();
+        adapter.notifyItemRangeInserted(0, podcasts.size());
     }
 
     @Override
     public void onImageView(int position) {
         startActivity(PodcastDetailActivity.getIntent(getActivity(), podcasts.get(position)));
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        super.onErrorResponse(error);
+        progressBar.setVisibility(View.GONE);
+        this.error.setVisibility(View.VISIBLE);
     }
 }
