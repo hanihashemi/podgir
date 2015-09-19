ddbfcceb0cd822e594985224ba435f6daad51375
Hani <jhanihashemi@gmail.com>
2015-09-20 00:57:01 +0430

    Player fragment design is complete

--- app/src/main/java/io/github/hanihashemi/podgir/fragment/PlayerFragment.java
+++ app/src/main/java/io/github/hanihashemi/podgir/fragment/PlayerFragment.java
@@ -1,26 +1,39 @@
 package io.github.hanihashemi.podgir.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.squareup.otto.Subscribe;
 
 import butterknife.Bind;
-import butterknife.OnClick;
+import io.github.hanihashemi.podgir.App;
 import io.github.hanihashemi.podgir.R;
 import io.github.hanihashemi.podgir.base.BaseFragment;
 import io.github.hanihashemi.podgir.model.Feed;
-import io.github.hanihashemi.podgir.service.MediaPlayerService;
+import io.github.hanihashemi.podgir.widget.AppPlayButton;
 import io.github.hanihashemi.podgir.widget.AppTextView;
+import timber.log.Timber;
 
 /**
  * Created by hani on 8/30/15.
  */
-public class PlayerFragment extends BaseFragment {
+public class PlayerFragment extends BaseFragment implements AppPlayButton.PlayListener {
 
     public static final String ARG_FEED = "feed";
     @Bind(R.id.name)
     AppTextView name;
     @Bind(R.id.title)
     AppTextView title;
+    @Bind(R.id.play)
+    AppPlayButton play;
+    @Bind(R.id.back_ten_seconds)
+    ImageButton backTenSeconds;
+
     private Feed feed;
+
 
     public static PlayerFragment getInstance(Feed feed) {
         Bundle bundle = new Bundle();
@@ -44,12 +57,48 @@ public class PlayerFragment extends BaseFragment {
     @Override
     protected void customizeUI() {
         super.customizeUI();
+        setRetainInstance(true);
         name.setText(feed.getPodcastName());
         title.setText(feed.getTitle());
+        play.setPlayListener(this);
+        backTenSeconds.setImageDrawable(new IconicsDrawable(getContext()).icon(GoogleMaterial.Icon.gmd_replay_10)
+                .color(Color.WHITE)
+                .sizeDp(50));
     }
 
-    @OnClick(R.id.play)
-    protected void onClickedPlay() {
-        getActivity().startService(MediaPlayerService.getIntent(getActivity(), feed));
+//        getActivity().startService(MediaPlayerService.getIntent(getActivity(), feed));
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        App.getInstance().getBus().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        App.getInstance().getBus().unregister(this);
+    }
+
+    protected void onPlayClicked() {
+
+    }
+
+    protected void onPauseClicked() {
+
+    }
+
+    protected void onFifteenSeconsClicked() {
+
+    }
+
+    @Subscribe
+    public void answerAvailable(String event) {
+        Timber.d("Salam " + event);
+    }
+
+    @Override
+    public void onClick(boolean isPlay) {
+
     }
 }
