91a179fca79522cc450f7465c0e09eb446a77f3b
Hani <jhanihashemi@gmail.com>
2015-11-13 16:29:05 +0330

    Fetching data in onCreate

--- app/src/main/java/io/github/hanihashemi/podgir/base/BaseSwipeFragment.java
+++ app/src/main/java/io/github/hanihashemi/podgir/base/BaseSwipeFragment.java
@@ -28,11 +28,6 @@ public abstract class BaseSwipeFragment<T> extends BaseFragment implements Swipe
         super.customizeUI();
         swipeRefresh.setColorSchemeResources(R.color.accent);
         swipeRefresh.setOnRefreshListener(this);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
         setSwipeRefresh(true);
         fetchData();
     }
